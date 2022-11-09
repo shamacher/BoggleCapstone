e0ade5ea8a12e8ce283995be6089d23e61aef4cf
shamacher <shamacher2@huskers.unl.edu>
2022-11-09 16:55:49 -0600

    Changed project to ASP.NET MVC

---
+++ BoggleApp/BoggleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoggleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
